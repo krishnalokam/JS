aaa200c43f0c7250ed876a2ecbfc9ac1021b181e
sivalokam <skkumarlokam@gmail.com>
2022-01-09 11:54:54 +0530

    complete valuess and variables +practice

---
+++ udemy/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,15 @@
+// LECTURE: Values and Variables
+/*
+1. Declare variables called 'country', 'continent' and 'population' and 
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+
+*/
+
+let country = "India";
+let continent = "Asia";
+let population = 1380000000;
+
+console.log("My Country is "+country);
+console.log("My Continent is "+continent);
+console.log("My Countrys population is "+population);
---
+++ udemy/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,20 @@
+let js = "amazing";
+// if (js === "amazing") alert("JavaScript is FUN!");
+
+console.log(48 + 53 + 27 + 128);
+
+console.log('Siva Lokam');
+console.log(23);
+
+let firstName="Siva"
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// let 1stYear = 1;
+
+let first_name = "Siva";
+let PI = 3.14;
+let myFirstJob = "Developer";
+let myCurrentJob = "Architect";
+
